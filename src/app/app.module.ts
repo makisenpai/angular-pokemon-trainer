0956e9992831cd84c0ab656e12d0e6203388e935
emilons <emilons@hotmail.com>
2021-09-28 14:01:11 +0200

    added pokedex-page and pokedex-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PokedexPageComponent } from './components/pokedex-page/pokedex-page.component';
+import { PokedexListComponent } from './components/pokedex-list/pokedex-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    NavbarComponent
+    NavbarComponent,
+    PokedexPageComponent,
+    PokedexListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/pokedex-list/pokedex-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex-list',
+  templateUrl: './pokedex-list.component.html',
+  styleUrls: ['./pokedex-list.component.css']
+})
+export class PokedexListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
