d369b9465b0b60484f6b0f2e2ccdc303e8ea7ec3
emilons <emilons@hotmail.com>
2021-09-28 13:57:02 +0200

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
