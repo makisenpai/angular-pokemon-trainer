f7f3a824aad40a9781a98d52b42d9be056a98337
emilons <emilons@hotmail.com>
2021-09-30 14:56:07 +0200

    gets/sets trainer pokemon from/in sessionstorage

--- src/app/components/pokedex-list/pokedex-list.component.ts
+++ src/app/components/pokedex-list/pokedex-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import {Pokemon} from "../../models/pokemon.model";
 
 @Component({
@@ -7,20 +8,26 @@ import {Pokemon} from "../../models/pokemon.model";
   styleUrls: ['./pokedex-list.component.css']
 })
 export class PokedexListComponent implements OnInit {
-
+  user: User = {
+    id: 0,
+    username: '',
+    pokemon: []
+  };
   pokemonList: Pokemon[] = [];
   constructor() { 
 
   }
 
   ngOnInit(): void {
-    let storage = sessionStorage.getItem('pokemonCache')
-    if (storage !== null) this.pokemonList = JSON.parse(storage);
+    let pokedex = sessionStorage.getItem('pokemonCache');
+    if (pokedex !== null) this.pokemonList = JSON.parse(pokedex);
+    let currentUser = sessionStorage.getItem('user')
+    if (currentUser !== null) this.user = JSON.parse(currentUser);
   }
 
-  //TODO: change handleCatch to add pokemon to trainer's list of pokemons and stuff
-  handleCatchPokemon = (pokemonId: number) => {
-    let indexOfPokemon = this.pokemonList.findIndex(x => x.id === pokemonId);
-    //this.pokemonList[indexOfPokemon].caught = true; //CHANGE TO ADD TO TRAINER LIST
+  handleCatchPokemon = (pokemon: Pokemon) => {
+    this.user.pokemon.push(pokemon)
+    sessionStorage.setItem('user', JSON.stringify(this.user));
+    //TODO: ALSO POST NEW TRAINER OBJECT TO API
   }
 }
