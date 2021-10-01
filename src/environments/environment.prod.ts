091771e944029a53cd6c73596faf744209d44a96
Tien Nguyen <tienguyen9@gmail.com>
2021-10-01 14:57:25 +0200

    added urls to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/",
+  pokeApiUrl: "https://pokeapi.co/api/v2/pokemon/",
+  userApiUrl: "https://tien-assignment-apis.herokuapp.com/trainers/",
+  apiKey: "t3wKgL0uNyz4kXRBOe7ggWmyrf50UKRUM4QSspTYFHZL20gAFIiEu16oFfIIdNvu"
 };
